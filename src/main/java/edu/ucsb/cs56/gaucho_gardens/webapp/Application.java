ac9fa7ee280293e11b35c63cd7cf908bb327994b
Daniel Gamliel <dgamliel@umail.ucsb.edu>
2018-12-02 21:19:43 -0800

    fixed my merge conflict

--- src/main/java/edu/ucsb/cs56/gaucho_gardens/webapp/Application.java
+++ src/main/java/edu/ucsb/cs56/gaucho_gardens/webapp/Application.java
@@ -3,6 +3,7 @@ package edu.ucsb.cs56.gaucho_gardens.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import edu.ucsb.cs56.gaucho_gardens.database.Database;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @SpringBootApplication
